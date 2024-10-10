7e184a8f0b5a7944c6ddbbe9060a61de632a15c2
Tanzilur31 <zneuro876@gmail.com>
2024-10-10 14:29:29 +0700

    main

--- lib/Nilai.dart
+++ lib/Nilai.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: 'Pengelompokan Nilai Siswa',
       theme: ThemeData.light(), // Tema terang
       darkTheme: ThemeData.dark(), // Tema gelap
